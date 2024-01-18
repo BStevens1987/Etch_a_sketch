305a0eb54de86ef96c64214cbdfe824771ed210e
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-17 22:36:18 -0800

    Create 16x16 div grid using java and flexbox

---
+++ java.js
@@ -0,0 +1,14 @@
+
+
+const container = document.querySelector('#container');
+
+for (let row =1; row <= 16; row++) {
+  const rowContainer = document.createElement('div');
+  rowContainer.id = 'row';
+  container.appendChild(rowContainer)
+  for (let column=1; column <=16; column++) {
+    const div = document.createElement('div');
+    div.id = 'cell';
+    rowContainer.appendChild(div);
+  };
+};
