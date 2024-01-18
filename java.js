0e5a1db7438f914de474b22e22596819edc53cc8
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-17 23:16:47 -0800

    Make createGrid function that creates grid based on size

--- java.js
+++ java.js
@@ -1,17 +1,21 @@
-
-
 const container = document.querySelector('#container');
 
-for (let row =1; row <= 16; row++) {
-  const rowContainer = document.createElement('div');
-  rowContainer.id = 'row';
-  container.appendChild(rowContainer)
-  for (let column=1; column <=16; column++) {
-    const div = document.createElement('div');
-    div.id = 'cell';
-    rowContainer.appendChild(div);
-    div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = 'black';
-    })
+function createGrid(size) {
+
+  for (let row =1; row <= size; row++) {
+    const rowContainer = document.createElement('div');
+    rowContainer.id = 'row';
+    container.appendChild(rowContainer)
+
+    for (let column=1; column <= size; column++) {
+      const div = document.createElement('div');
+      div.id = 'cell';
+      rowContainer.appendChild(div);
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = 'black';
+      })
+    };
   };
 };
+
+createGrid(16);
