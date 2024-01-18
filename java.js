9ff16afabfbd18d2a63e716431870c7e28ce0dd3
Brian Stevenson <81999388+BStevens1987@users.noreply.github.com>
2024-01-18 00:27:49 -0800

    Create choose grid size button and link to createGrid function

--- java.js
+++ java.js
@@ -1,11 +1,13 @@
 const container = document.querySelector('#container');
 
 function createGrid(size) {
-
+  const subContainer = document.createElement('div');
+  subContainer.id = 'subcontainer';
+  container.appendChild(subContainer);
   for (let row =1; row <= size; row++) {
     const rowContainer = document.createElement('div');
     rowContainer.id = 'row';
-    container.appendChild(rowContainer)
+    subContainer.appendChild(rowContainer)
 
     for (let column=1; column <= size; column++) {
       const div = document.createElement('div');
@@ -19,3 +21,10 @@ function createGrid(size) {
 };
 
 createGrid(16);
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', () => {
+  const input = prompt("Enter a size between 1 and 100: ");
+  container.removeChild(document.querySelector('#subcontainer'));
+  createGrid(input);
+});
